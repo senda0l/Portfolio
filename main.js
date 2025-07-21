696cf258627d9d4998f3eb6f0f2099d4c5b4f495
senda0l <hairyfobbit@gmail.com>
2025-07-22 01:25:42 +0500

    change

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-const
-    switcher = document.querySelector('.slider'),
-    body = document.body,
-    img = document.querySelectorAll('.dark1'),
-    gmail=document.querySelector('.mail'),
-    imgs=document.querySelectorAll('.light1');
+const switcher = document.querySelector(".slider"),
+  body = document.body,
+  img = document.querySelectorAll(".dark1"),
+  gmail = document.querySelector(".mail"),
+  imgs = document.querySelectorAll(".light1");
+sections = document.querySelectorAll("section");
+navLinks = document.querySelectorAll(".nav-link");
 
-switcher.addEventListener('click',()=>{
-    body.classList.toggle('dark');
+switcher.addEventListener("click", () => {
+  body.classList.toggle("dark");
 
-    gmail.classList.toggle('active');
-    
-    img.forEach(darkimg => {
-        darkimg.classList.toggle('active')
+  gmail.classList.toggle("active");
+
+  img.forEach((darkimg) => {
+    darkimg.classList.toggle("active");
+  });
+
+  imgs.forEach((lightimg) => {
+    lightimg.classList.toggle("light");
+  });
+
+});
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      const link = document.querySelector(`.nav-link[href="#${id}"]`);
+      if (entry.isIntersecting) {
+        navLinks.forEach((link) => link.classList.remove("active"));
+        if (link) link.classList.add("active");
+      }
     });
-    
-    imgs.forEach(lightimg => {
-        lightimg.classList.toggle('light')
-    });
-    
-    const isDark = body.contains('dark')+img.contains('active');
-    
-})
+  },
+  {
+    threshold: 0.9,
+  }
+);
+
+sections.forEach(section => {
+    observer.observe(section)
+});
